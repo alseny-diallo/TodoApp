a9796802817081aa995f89d09fbc8555d0851f39
alseny-diallo <alsenydiallo0409@gmail.com>
2022-03-11 11:24:50 +0000

    initial commit

---
+++ App.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+import Header from './components/Header';
+
+const App = () => {
+  const [todos, setTodos] = useState([
+    {title: 'to project', key: '1'},
+    {title: 'learning angular', key: '2'},
+    {title: 'learning reactjs', key: '3'},
+    {title: 'learning react-native', key: '4'},
+    {title: 'learning vuejs', key: '5'},
+  ]);
+  return (
+    <View style={styles.container}>
+      <Header />
+      <View style={styles.content}>
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({item}) => <Text>{item.title}</Text>}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  content: {padding: 40},
+  list: {marginTop: 20},
+});
+
+export default App;
