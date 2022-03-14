62713d58101111273dbbbe77bc62af79f4c6d494
alseny-diallo <alsenydiallo0409@gmail.com>
2022-03-14 07:52:25 +0000

    updating content of appjs file

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, FlatList} from 'react-native';
+import {View, StyleSheet, FlatList} from 'react-native';
 import Header from './components/Header';
 import TodoItem from './components/TodoItem';
+import AddTodo from './components/AddTodo';
 
 const App = () => {
   const [todos, setTodos] = useState([
@@ -13,14 +14,20 @@ const App = () => {
   ]);
   const pressHandler = key => {
     setTodos(prevTodos => {
-      return prevTodos.filter(todo => todo.key != key);
+      return prevTodos.filter(todo => todo.key !== key);
     });
   };
 
+  const submitHandler = text => {
+    setTodos(prevTodos => {
+      return [{title: text, key: Math.random().toString()}, ...prevTodos];
+    });
+  };
   return (
     <View style={styles.container}>
       <Header />
       <View style={styles.content}>
+        <AddTodo submitHandler={submitHandler} />
         <View style={styles.list}>
           <FlatList
             data={todos}
